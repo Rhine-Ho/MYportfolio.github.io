e75947f433eb3d8fdbb52d64715a4d5ea5b37410
Rhine-Ho <mao07033@gmail.com>
2022-12-15 15:37:15 +0800

    Create Animate.js

---
+++ doc/content/HooChuu/Animate.js
@@ -0,0 +1,18 @@
+
+const canvas = document.createAttribute('canvas');
+canvas.width = 512;
+canvas.height = 512;
+
+const ctx = canvas.getContext('2d');
+
+const img = new Image();
+
+img.onload = (e) => {
+
+}
+
+doucument.querySelectorAll('canvas img').forEach(function(){
+
+
+    
+})
