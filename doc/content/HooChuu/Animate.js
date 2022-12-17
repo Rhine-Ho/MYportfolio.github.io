6fd06bf82b1c0086b2776c6cb4b3591943588c4f
Rhine-Ho <mao07033@gmail.com>
2022-12-17 15:25:56 +0800

    Update Animate.js

--- doc/content/HooChuu/Animate.js
+++ doc/content/HooChuu/Animate.js
@@ -1,18 +1,57 @@
 
-const canvas = document.createAttribute('canvas');
+/**const canvas = document.createAttribute('canvas');
 canvas.width = 512;
 canvas.height = 512;
 
 const ctx = canvas.getContext('2d');
 
-const img = new Image();
-
 img.onload = (e) => {
 
 }
 
-doucument.querySelectorAll('canvas img').forEach(function(){
+doucument.querySelectorAll('canvas').forEach(function(){
 
 
     
+})**/
+const bk = document.querySelector('#background');
+
+/***---------background style----------***/
+//點擊事件轉換背景 , 滑鼠背景交換 ,
+doucument.querySelectorAll('.breakfast').forEach(e =>{
+    e.style.backgroundimg = bk.style.backgroundimg;
+    e.addEventListener('click',funtion(),{
+        window,onclick = funtion(i){
+                x = i.clientX;
+                y = i.clientY;
+                document.getElementById("cursor").style.top = y-10+"px";
+                document.getElementById("cursor").style.left = x+10+"px";
+        };
+        
+
+    });
+window.background.style.fill = document.querySelector('.HooChuuA');
+window.background.style.stroke = document.querySelector('.HooChuuA');
+window.background.style.strokeDasharray = document.querySelector('.HooChuuA');
+});
+/***---------background style----------***/
+/***-------------------***/
+
+/***--------cursor-----------***/
+
+const mouse = doucument.querySelector('#cursor');
+
+//增加點及效果 ，
+document.querySelector('#cursor').forEach(e =>{
+    e.addEventListener('click',funtion(),{
+        window,onclick = funtion(i){
+                x = i.clientX;
+                y = i.clientY;
+                document.getElementById("cursor").style.top = y-10+"px";
+                document.getElementById("cursor").style.left = x+10+"px";
+        };
+
 })
+
+                cursor.style.cursor = document.getElementById()
+cursor.addEventListener('click',funtion(),{
