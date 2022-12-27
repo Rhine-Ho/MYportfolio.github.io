87577fad484195eda9b3539b135512f61df4cb11
Rhine-Ho <mao07033@gmail.com>
2022-12-27 19:54:17 +0800

    Delete pagestyle.js

--- doc/javascript/pagestyle.js
+++
@@ -1,6 +0,0 @@
-const colorButtons = [...document.querySelectorAll('.colorButton')];
-document.addEventListener('click', e => {
-    if (!colorButtons.includes(e.target)) return;
-    document.body.style.background = e.target.textContent;
-});
-
