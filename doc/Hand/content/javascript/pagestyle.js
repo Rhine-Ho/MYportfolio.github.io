c44c177fe0db89647c56efa4a40bdabd5c3de868
Rhine-Ho <mao07033@gmail.com>
2022-12-27 19:53:48 +0800

    Create pagestyle.js

---
+++ doc/Hand/content/javascript/pagestyle.js
@@ -0,0 +1,6 @@
+const colorButtons = [...document.querySelectorAll('.colorButton')];
+document.addEventListener('click', e => {
+    if (!colorButtons.includes(e.target)) return;
+    document.body.style.background = e.target.textContent;
+});
+
